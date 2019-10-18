33e6647838c4236b05eb1a3553375fed54bbd0b5
Paquan <andy.bestvater@gmx.de>
2019-10-18 17:26:33 +0200

    clean up

--- src/fake-backend/fake-backend.js
+++ src/fake-backend/fake-backend.js
@@ -1,12 +1,12 @@
 var dashboardData = require('./dashboard.json');
 var users = require('./users.json');
 // TODO envirioment
-var enviroment = 'FAKE_BACKEND'
+var enviroment = 'FAKE_BACKEND';
 
 export function configureFakeBackend() {
   console.log('configureFakeBackend ...');
   let realFetch = window.fetch;
-  window.fetch = function (url, opts) {
+  window.fetch = function(url, opts) {
     return new Promise((resolve, reject) => {
       console.log('called: ', url);
       if (enviroment !== 'FAKE_BACKEND') {
@@ -15,8 +15,8 @@ export function configureFakeBackend() {
       }
 
       if (url.endsWith('/api/auth/login') && opts.method === 'POST') {
-          doFakeLogin();
-         return;
+        doFakeLogin();
+        return;
       }
       if (url.endsWith('/api/dashboard') && opts.method === 'GET') {
         getDashboardData(resolve);
@@ -24,11 +24,15 @@ export function configureFakeBackend() {
       }
       realFetch(url, opts).then(response => resolve(response));
     });
-  }
+  };
 }
 
 function getDashboardData(resolve) {
-  resolve({ok: true, status: 200, json: () => Promise.resolve(dashboardData)});
+  resolve({
+    ok: true,
+    status: 200,
+    json: () => Promise.resolve(dashboardData),
+  });
 }
 
 function doFakeLogin() {
@@ -36,22 +40,32 @@ function doFakeLogin() {
   console.log('FAKT-BACKEND: /api/auth/login', params);
   // find if any user matches login credentials
   let filteredUsers = users.filter(user => {
-    return user.username === params.username && user.password === params.password;
+    return (
+      user.username === params.username && user.password === params.password
+    );
   });
 
   if (filteredUsers.length) {
     // if login details are valid return user details and fake jwt token
     let user = filteredUsers[0];
     const jsondata = {
-      user: JSON.stringify(user)
+      user: JSON.stringify(user),
     };
     const accessToken = jwtUtils.createToken(jsondata);
     let responseJson = {
-      token: accessToken
+      token: accessToken,
     };
-    resolve({ok: true, status: 200, json: () => Promise.resolve(responseJson)});
+    resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve(responseJson),
+    });
   } else {
-    resolve({ok: true, status: 200, json: () => Promise.resolve(errors.loginFailed)});
+    resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve(errors.loginFailed),
+    });
   }
 }
 
@@ -59,7 +73,7 @@ function getParams(url) {
   var regex = /[?&]([^=#]+)=([^&#]*)/g,
     params = {},
     match;
-  while (match = regex.exec(url)) {
+  while ((match = regex.exec(url))) {
     params[match[1]] = match[2];
   }
   return params;
